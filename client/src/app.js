02dc5d86a42ba147186f58efef037742523ba33b
Mudrock1776 <dhughes1582@gmail.com>
2023-11-26 20:38:16 -0500

    Main updates

--- client/src/app.js
+++ client/src/app.js
@@ -1,22 +1,24 @@
 import React from "react";
 import { Route, Routes } from "react-router-dom";
-import Main from "./components/main"; // Use PascalCase for component names
-import Nav from "./components/Nav";
 
-import WorkStation from "./components/workstation";
 
 //Components
 import Login from "./components/Login";
+import Alert from "./components/Alert.tsx";
+import Button from "./components/Button.tsx";
+import ListGroup from "./components/ListGroup.tsx";
+import Processes from "./processes.tsx";
+import Main from "./components/main"; // Use PascalCase for component names
+import Nav from "./components/Nav";
+import WorkStation from "./components/workstation";
+import Logout from "./components/logout.js";
 
 function getToken(){
     const tokenString = sessionStorage.getItem('token');
     const userToken = JSON.parse(tokenString);
     return userToken?.token
 }
-import Alert from "./components/Alert.tsx";
-import Button from "./components/Button.tsx";
-import ListGroup from "./components/ListGroup.tsx";
-import Processes from "./processes.tsx";
+
 
 const App = () => {
     const token = getToken();
@@ -29,8 +31,9 @@ const App = () => {
             <Routes>
                 <Route path="/" element={<Main />} />
                 <Route path="/main" element={<Main />} />
-                <Route exact path="/" element={<WorkStation />} />
-                <Route exact path="/" element={<Processes></Processes>} />
+                <Route exact path="/workstation" element={<WorkStation />} />
+                <Route exact path="/process" element={<Processes></Processes>} />
+                <Route exact path="/logout" element={<Logout />} />
             </Routes>
         </div>
     );
