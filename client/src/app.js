138aa449a5ad1a55a2b00b45bfa77868f6fb524a
borisnikolovx <114265131+borisnikolovx@users.noreply.github.com>
2023-11-26 20:11:52 -0500

    Update app.js

--- client/src/app.js
+++ client/src/app.js
@@ -2,9 +2,19 @@ import React from "react";
 import { Route, Routes } from "react-router-dom";
 
 //Components
+import Login from "./components/Login";
 
+function getToken(){
+    const tokenString = sessionStorage.getItem('token');
+    const userToken = JSON.parse(tokenString);
+    return userToken?.token
+}
 
 const App = () => {
+    const token = getToken();
+    if(!token) {
+        return <Login />
+    }
     return(
         <div>
             <Routes>
