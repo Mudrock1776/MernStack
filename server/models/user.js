0153d99ff9137e90df68fef935b7f6b56e46fc59
borisnikolovx <114265131+borisnikolovx@users.noreply.github.com>
2023-11-27 21:56:34 -0500

    Update user.js

--- server/models/user.js
+++ server/models/user.js
@@ -2,6 +2,7 @@ const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
 const user = new Schema ({
+    email: {type: String, required: true},
     username: {type: String, required: true},
     password: {type: String, required: true},
 });
